382a3d4b2c004229af07ddcbd28292fbbeb308ca
filippodanesi <filippo.danesi93@gmail.com>
2024-12-06 09:00:07 +0100

    sa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import jopSoftwarecookieconsent from '@jop-software/astro-cookieconsent';
 import react from '@astrojs/react';
@@ -9,6 +9,7 @@ export default defineConfig({
   site: 'https://www.serp-secrets.com',
   output: 'server',
   adapter: vercel({
+    webAnalytics: { enabled: false },
     isr: { expiration: 60 * 60 * 24 },
   }),
   trailingSlash: 'always',
