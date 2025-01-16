c5191dac24964e285624a6d5af08c48ac2199353
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2025-01-16 19:53:02 +0100

    sa

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,8 @@ module.exports = {
     darkMode: 'class',
     theme: {
         fontFamily: {
-            sans: ['Geist', ...defaultTheme.fontFamily.sans],
-            serif: ['Gelasio', ...defaultTheme.fontFamily.serif]
+            sans: ['Inter', ...defaultTheme.fontFamily.sans],
+            serif: ['Newsreader', ...defaultTheme.fontFamily.serif]
         },
         extend: {
             textColor: {
