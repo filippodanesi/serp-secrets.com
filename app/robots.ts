084b3b37d3fb3e38f620451d8affbf5ff355ab01
filippodanesi <filippo.danesi93@gmail.com>
2025-12-12 18:15:59 +0100

    chore: remove host directive from robots.ts configuration.

--- app/robots.ts
+++ app/robots.ts
@@ -10,6 +10,5 @@ export default function robots(): MetadataRoute.Robots {
       },
     ],
     sitemap: 'https://www.serp-secrets.com/sitemap.xml',
-    host: 'https://www.serp-secrets.com',
   }
 }
